0c2940fec716ec049d828bac2d29ceefd88b4b76
Danny Weiner <danny@phantomnet.net>
2014-02-19 10:45:30 -0800

    Added the twitter bootstrap admin dashboard

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -1,23 +1,7 @@
-<div class="wrapper">
-	<!-- Wrapper will wrap the content within a centered block -->
-	<div id="footer-left" class="footer-column">
-		<div id="copyright">
-		@section('copyright')
-			Copyright 2013-2014
-		@show
-		</div><!--div#copyright -->
-	</div><!-- div#footer-left -->
-	<div id="footer-center" class="footer-column">
-		@section('footer-center')
-
-		@show
-	</div><!-- div#footer-center -->
-	<div id="footer-right" class="footer-column">
-		<ul id="nav-menu" class="menu">
-			<li class="menuitem"><a href="{{ URL::to('/') }}">Home</a></li><!-- li.menuitem -->
-			@yield('footer-nav-menu')
-			<li class="menuitem"><a href="#">Logout</a></li><!-- li.menuitem -->
-		</ul><!-- ul#nav-menu -->
-	</div><!-- div#footer-right -->
-	<div class="clear"></div><!-- div.clear -->
-</div><!-- div.wrapper -->
+<!-- Bootstrap core JavaScript
+================================================== -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
+<script src="{{ URL::asset('assets/js/docs.min.js') }}"></script>
+<!-- End Twitter Bootstrap includes -->
--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -1,21 +1,21 @@
-<!-- Sticky footer fix for IE -->
-<!--[if !IE 7]>
-<style type="text/css">
-	#wrap {display:table;height:100%}
-</style>
-<![endif]-->
-<!-- Primary CSS Files -->
-<link href="<?php echo asset('assets/css/site.css'); ?>" media="all" rel="stylesheet" type="text/css" />
-<!-- Twitter Bootstrap 3.0.3 -->
-<!-- TWB CSS-->
-<link href="<?php echo asset('assets/css/bootstrap.min.css'); ?>" media="all" rel="stylesheet" type="text/css" />
-<link href="<?php echo asset('assets/css/bootstrap-theme.min.css'); ?>" media="all" rel="stylesheet" type="text/css" />
-<!-- TWB Javascript -->
-<script src="<?php echo asset('assets/js/bootstrap.min.js'); ?>" type="text/javascript"></script>
-<!-- Primary Site Javascript -->
-<script src="<?php echo asset('assets/js/site.js'); ?>" type="text/javascript"></script>
 <!-- Meta Tags -->
 <meta charset='utf-8'>
-<!-- -->
-<link rel="icon" type="image/x-icon" href="<?php echo asset('favicon.ico'); ?>" />
-<title><?php echo $title; ?></title>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- Twitter Bootstrap 3.0.3 -->
+<!-- TWB CSS-->
+<link href="{{ URL::asset('assets/css/bootstrap.min.css') }}" rel="stylesheet">
+<!-- Primary CSS Files -->
+<link href="{{ URL::asset('assets/css/site.css') }}" media="all" rel="stylesheet" type="text/css" />
+<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!--[if lt IE 9]>
+  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<!-- Primary Site Javascript -->
+<script src="{{ URL::asset('assets/js/site.js') }}" type="text/javascript"></script>
+<!-- Favicon -->
+<link rel="shortcut icon" href="{{ URL::asset('favicon.ico') }}">
+<title>@yield('title', 'Dashboard')</title>
